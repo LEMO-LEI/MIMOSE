173d84b3b4eb8279301f66689ca5cdcf1bfcc101
Administrator <Administrator@USER-20170614TN>
2017-07-08 00:59:45 +0800

    hello  git

---
+++ First/src/net/lemo/first/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.lemo.first;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
