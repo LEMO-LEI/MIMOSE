f30644f9dd9b73b918b9c4c1e1b2b24d7aaa09aa
tomorrow <tomorrowzyong@gmail.com>
2017-07-08 12:23:42 +0800

    SS

---
+++ sjddsj/src/main/java/net/lemo/sjddsj/App.java
@@ -0,0 +1,13 @@
+package net.lemo.sjddsj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
